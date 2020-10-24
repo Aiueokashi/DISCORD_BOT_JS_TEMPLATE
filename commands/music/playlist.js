53ece182a6f6f8062d678066ed6354d75b100b72
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-10-24 10:23:27 +0900

    Update playlist.js

--- commands/music/playlist.js
+++ commands/music/playlist.js
@@ -3,5 +3,117 @@ const YouTubeAPI = require("simple-youtube-api");
 const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
 
 module.exports = class PlaylistCommand extends Command {
-    constructer()
+	constructor() {
+		super('playlist', {
+			aliases: ['playlist', 'pl'],
+			category: 'music',
+			description: 'play playlist from youtube'
+		});
+        
+    async exec(message, args) {
+    const { PRUNING } = require(".../config.json");
+    const { channel } = message.member.voice;
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (serverQueue && channel !== message.guild.me.voice.channel)
+      return message.reply(`${message.client.user}と同じチャンネルにいてね`).catch(console.error);
+
+    if (!args.length)
+      return message
+        .reply(`Usage: ${message.client.prefix}playlist <YouTube Playlist URL | Playlist Name>`)
+        .catch(console.error);
+    if (!channel) return message.reply("ボイチャに接続してね").catch(console.error);
+
+    const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT"))
+      return message.reply("接続できません\n必要な権限:CONNECT");
+    if (!permissions.has("SPEAK"))
+      return message.reply("再生できません\n必要な権限:SPEAK");
+
+    const search = args.join(" ");
+    const pattern = /^.*(youtu.be\/|list=)([^#\&\?]*).*/gi;
+    const url = args[0];
+    const urlValid = pattern.test(args[0]);
+
+    const queueConstruct = {
+      textChannel: message.channel,
+      channel,
+      connection: null,
+      songs: [],
+      loop: false,
+      volume: 100,
+      playing: true
+    };
+
+    let song = null;
+    let playlist = null;
+    let videos = [];
+
+    if (urlValid) {
+      try {
+        playlist = await youtube.getPlaylist(url, { part: "snippet" });
+        videos = await playlist.getVideos(MAX_PLAYLIST_SIZE || 10, { part: "snippet" });
+      } catch (error) {
+        console.error(error);
+        return message.reply("playlistが見つかりませんでした :(").catch(console.error);
+      }
+    } else {
+      try {
+        const results = await youtube.searchPlaylists(search, 1, { part: "snippet" });
+        playlist = results[0];
+        videos = await playlist.getVideos(MAX_PLAYLIST_SIZE || 10, { part: "snippet" });
+      } catch (error) {
+        console.error(error);
+        return message.reply("playlistが見つかりませんでした:(").catch(console.error);
+      }
+    }
+
+    videos.forEach((video) => {
+      song = {
+        title: video.title,
+        url: video.url,
+        duration: video.durationSeconds
+      };
+
+      if (serverQueue) {
+        serverQueue.songs.push(song);
+        if (!PRUNING)
+          message.channel
+            .send(`✅ **${song.title}** を${message.author}が追加したよ`)
+            .catch(console.error);
+      } else {
+        queueConstruct.songs.push(song);
+      }
+    });
+
+    let playlistEmbed = new MessageEmbed()
+      .setTitle(`${playlist.title}`)
+      .setURL(playlist.url)
+      .setColor("RANDOM")
+      .setTimestamp();
+
+    if (!PRUNING) {
+      playlistEmbed.setDescription(queueConstruct.songs.map((song, index) => `${index + 1}. ${song.title}`));
+      if (playlistEmbed.description.length >= 2048)
+        playlistEmbed.description =
+          playlistEmbed.description.substr(0, 2007) + "\nプレイリストが長すぎます...";
+    }
+
+    message.channel.send(`${message.author} playlist再生開始`, playlistEmbed);
+
+    if (!serverQueue) message.client.queue.set(message.guild.id, queueConstruct);
+
+    if (!serverQueue) {
+      try {
+        queueConstruct.connection = await channel.join();
+        await queueConstruct.connection.voice.setSelfDeaf(true);
+        play(queueConstruct.songs[0], message);
+      } catch (error) {
+        console.error(error);
+        message.client.queue.delete(message.guild.id);
+        await channel.leave();
+        return message.channel.send(`チャンネルに接続できません: ${error}`).catch(console.error);
+      }
+    }
+  }
 }
