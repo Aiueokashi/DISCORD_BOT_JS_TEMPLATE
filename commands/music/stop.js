5e6f40c3e303e2c410abf31ebbab0ab9e57b23ca
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-10-24 10:13:53 +0900

    Create stop.js

---
+++ commands/music/stop.js
@@ -0,0 +1,22 @@
+const { canModifyQueue } = require("../util/MusicUtils");
+
+
+module.exports = class StopCommand extends Command{
+	constructor() {
+		super('stop', {
+			aliases: ['stop', 'st'],
+			category: 'music',
+			description: 'stop music playing'
+		});
+	}
+  exec(message) {
+    const queue = message.client.queue.get(message.guild.id);
+    
+    if (!queue) return message.reply("何も再生してないよ").catch(console.error);
+    if (!canModifyQueue(message.member)) return;
+
+    queue.songs = [];
+    queue.connection.dispatcher.end();
+    queue.textChannel.send(`${message.author} ⏹ 停止`).catch(console.error);
+  }
+};
