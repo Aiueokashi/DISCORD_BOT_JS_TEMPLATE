3d1c8ec8a63ec6c9080ec5953a650bc9e657555a
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-10-24 09:56:31 +0900

    Create eval.js

---
+++ commands/owner/eval.js
@@ -0,0 +1,39 @@
+const { Command } = require('discord-akairo');
+const { inspect } = require('util');
+const { stripIndents } = require('common-tags');
+const path = require('path');
+client = new discord.Client()
+module.exports = class EvalCommand extends Command {
+	constructor() {
+		super('eval', {
+			aliases: ['eval','evaluate'],
+			category: 'admin',
+			description: 'An evaluate command',
+      ownerOnly: true,
+      typing:true,
+      cooldown:2000,
+		});
+	}
+async exec(message) {
+  const prefix = process.env.AUTO_PREFIX
+  const args = message.content.slice(prefix.length+4).split(' ')
+		let evaled;
+		var sourceStr = message.content;
+		var code = sourceStr.slice(prefix.length+4);
+		try {
+			evaled = await eval(args.join(' '));
+			message.react('✅');
+			console.log(inspect(evaled));
+		} catch (error) {
+			console.error(error);
+			var errormsg = msg.channel.send({
+				embed: {
+					color: 0x00ae86,
+					title: ':warning:error',
+					description: 'code:```' + code + '```error:```' + error + '```'
+				}
+			});
+			message.react('❎');
+		}
+}
+};
