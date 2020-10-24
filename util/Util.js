c97dc04c53956d8f3cac926570442ed0d36c253a
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-10-24 09:15:54 +0900

    Create Util.js

---
+++ util/Util.js
@@ -0,0 +1,30 @@
+class Util {
+	static shorten(text, maxLen = 2000) {
+		return text.length > maxLen ? `${text.substr(0, maxLen - 3)}...` : text;
+	}
+
+	static trimArray(arr, maxLen = 10) {
+		if (arr.length > maxLen) {
+			const len = arr.length - maxLen;
+			arr = arr.slice(0, maxLen);
+			arr.push(`${len} more...`);
+		}
+		return arr;
+	}
+
+	static firstUpperCase(text) {
+		return `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
+	}
+
+	static escapeRegex(str) {
+		return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+	}
+
+	static base64(text, mode = 'encode') {
+		if (mode === 'encode') return Buffer.from(text).toString('base64');
+		if (mode === 'decode') return Buffer.from(text, 'base64').toString('utf8') || null;
+		throw new TypeError(`${mode} base64 mode is not supported`);
+	}
+}
+
+module.exports = Util;
