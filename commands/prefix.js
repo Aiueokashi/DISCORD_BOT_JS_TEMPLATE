596c3e2f9de1cce4b478e18d378ea9771d66d31a
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-10-27 12:37:14 +0900

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,1 +1,59 @@
-
+const { Command } = reqiure('discord-akairo');
+module.exports = class PrefixCommand extends Command {
+	constructor() {
+		super('setprefix', {
+			aliases: ['sp','prefix','setprefix'],
+			category: 'admin',
+			description: 'Set bot prefix',
+      ownerOnly: true,
+      cooldown:2000,
+		});
+	}
+exec(message){
+if (prefix === null) prefix = mainprefix;
+    if(message.content.startsWith(prefix + 'config')) {
+        let args = message.content.split(' ');
+      if (!message.member.hasPermission("MANAGE_GUILD")&&!owners.includes(message.author.id))
+        return message.channel.send(
+          "You need `MANAGE GUILD` to configure the server settings!"
+        );
+      let content = args[1];
+      if (!content) {
+        let kk = new Discord.MessageEmbed()
+          .setColor(`RANDOM`)
+          .setTitle(`${message.guild.name} Settings`)
+          .setDescription(`
+        \`\`\`${prefix}config [key] [value]\`\`\`
+        \`joinMessageChannel\`,\`leaveMessageChannel\`,\`prefix\`,\`show\`,\`reset-invites <@User>\`,\`resetall-invites\`,\`add-invites\``
+          )
+          .setFooter(message.guild.name, client.user.displayAvatarURL());
+       return message.channel.send(kk);
+      }
+      if (content.toLowerCase() === "prefix") {
+        let prefixembed = new Discord.MessageEmbed()
+             .setColor(`RANDOM`)
+          .setTitle(`**SetPrefix**`)
+          .setDescription(
+            `This Config is Currently Set.
+        Use \`\`\`${prefix}config prefix <value>\`\`\` to change it.
+        **Current Value**
+        \`\`\`${prefix}\`\`\`
+        `
+          )
+          .setFooter(message.guild.name, client.user.displayAvatarURL());
+        let newprefix = args[2];
+  
+        if (!newprefix) {
+          return message.channel.send(prefixembed);
+        }
+        let changedprefix = new Discord.MessageEmbed()
+          .setTitle(`**Prefix Updated**`)
+               .setColor(`RANDOM`)
+          .setDescription(
+            `** Old Value **\n${prefix}\n** New Value **\n${newprefix}`
+          )
+          .setFooter(message.guild.name, client.user.displayAvatarURL());
+        db.delete(`guildprefix_${message.guild.id}`);
+        db.set(`guildprefix_${message.guild.id}`, newprefix);
+        return message.channel.send(changedprefix);
+      }
